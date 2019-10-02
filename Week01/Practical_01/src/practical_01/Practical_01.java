52693c06bec303ca0b0035d1687f062a44d98e47
(s) James Bridgman <james.bridgman@students.plymouth.ac.uk>
2019-10-02 11:34:26 +0100

    First Commit

---
+++ Week01/Practical_01/src/practical_01/Practical_01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical_01;
+
+/**
+ *
+ * @author jbridgman2
+ */
+public class Practical_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello World!");
+       System.exit(0);
+    }
+    
+}
