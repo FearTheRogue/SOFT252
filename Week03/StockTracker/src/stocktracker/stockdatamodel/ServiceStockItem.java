6c4463acfde31179711c38275721f25f24644302
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-11-04 16:03:08 +0000

    Finsihed Practical 3 exercise

--- Week03/StockTracker/src/stocktracker/stockdatamodel/ServiceStockItem.java
+++ Week03/StockTracker/src/stocktracker/stockdatamodel/ServiceStockItem.java
@@ -12,11 +12,16 @@ package stocktracker.stockdatamodel;
 public class ServiceStockItem extends StockItem {
     
     public ServiceStockItem(String name){
-        this.name = name;
+        this.name = name; 
     }
     
     public ServiceStockItem(String name, Integer qty){
         this.name = name;
         this.quantityInStock = qty;
+    }
+
+    @Override
+    public Boolean isInStock() {
+        return true;
     }
 }
