2cbe570797041a3508255b69216cb62130b91d04
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-11-04 12:49:18 +0000

    Finsihed Polymorphic 7

---
+++ Week03/StockTracker/src/stocktracker/stockdatamodel/StockItem.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stocktracker.stockdatamodel;
+
+/**
+ *
+ * @author James
+ */
+public class StockItem {
+    private String name = "UNKNOWN";
+    private Integer quantityInStock = 0;
+    private Double sellingPrice = 1000000.00;
+    private Double costPrice = 1000000.00;
+
+    public Boolean isInStock(){
+        Boolean isStock = false;
+        if(this.quantityInStock >0){
+            inStock = true;
+        }
+        return inStock;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name != null && !name.isEmpty()){
+        this.name = name;
+        }
+    }
+
+    public Integer getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(Integer quantityInStock) {
+        if(quantityInStock != null && quantityInStock >= 0){
+        this.quantityInStock = quantityInStock;
+        }
+    }
+
+    public Double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public Double getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(Double costPrice) {
+        if(sellingPrice != null && sellingPrice >= 
+                this.costPrice && sellingPrice >= 0){
+        this.costPrice = costPrice;
+        }
+    }
+    
+    
+}
